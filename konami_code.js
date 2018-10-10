197816e0ff66fb5283933d0cd7b358c1e64f8086
derrick b <dbozich1@gmail.com>
2018-10-10 01:41:35 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,46 +1,46 @@
-// const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
-//
-// function init() {
-//   let index = 0;
-//
-//   document.body.addEventListener('keydown', (event) => {
-//     console.log(event.which);
-//
-//     let input = parseInt(event.which || e.detail);
-//     if (code[index] === input) {
-//       console.log(index);
-//       index ++;
-//       if (index  == code.length) {
-//
-//         alert("Hurray!");
-//         index = 0;
-//       }
-//
-//
-//     } else {
-//       index = 0;
-//     }
-//   });
-// }
-//
-// init();
+const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+
+function init() {
+  let index = 0;
+
+  document.body.addEventListener('keydown', (event) => {
+    console.log(event.which);
+
+    let input = parseInt(event.which || e.detail);
+    if (code[index] === input) {
+      console.log(index);
+      index ++;
+      if (index  == code.length) {
+
+        alert("Hurray!");
+        index = 0;
+      }
+
+
+    } else {
+      index = 0;
+    }
+  });
+}
+
+init();
 
 
 
 // const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
-// 
+//
 // function init() {
 //   let index = 0;
-// 
+//
 //   document.body.addEventListener('keydown', function(e) {
 //     const key = parseInt(e.which || e.detail);
-// 
+//
 //     if (code[index] === key) {
 //       index++;
-// 
+//
 //       if (index === code.length) {
 //         alert('Hurray!');
-// 
+//
 //         index = 0;
 //       }
 //     } else {
@@ -48,5 +48,5 @@
 //     }
 //   });
 // }
-// 
+//
 // init();
